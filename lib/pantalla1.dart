f00e22f187a6733e197ce3a0ec4c2f952349de8a
Rodriguezb128 <143763162+Rodriguezb128@users.noreply.github.com>
2024-03-01 13:33:43 -0700

    Update pantalla1.dart

--- lib/pantalla1.dart
+++ lib/pantalla1.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:rodriguez/pantalla1.dart';
 import 'package:rodriguez/pantalla2.dart';
 
-class Pantalla1 extends StatelessWidget {
-  const Pantalla1({Key? key}) : super(key: key);
+void main() => runApp(RutasPaginas());
+
+class RutasPaginas extends StatelessWidget {
+  const RutasPaginas({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Pantalla1 Valeria Rodriguez"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Vamos a la pantalla2"),
-          onPressed: () {
-            Navigator.pushNamed(context, "/pantalla2",
-                arguments: "Mensaje desde la pantalla1");
-          },
-        ),
-      ),
-    );
-  } //widget
-} //Fiin clase pantalla 1
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      debugShowCheckedModeBanner: false,
+      title: "App entre pagina",
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Pantalla1(),
+        "/pantalla2": (context) => Pantalla2(),
+      }, //navegacion entre paginas
+    ); //fin materal
+  } //clase ruta pagina
+} //fin RutasPaginas
