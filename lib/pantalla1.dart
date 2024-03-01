bd26cfb19947769977e2e4b28244e3a5b1eddb00
Rodriguezb128 <143763162+Rodriguezb128@users.noreply.github.com>
2024-03-01 13:34:03 -0700

    Update pantalla1.dart

--- lib/pantalla1.dart
+++ lib/pantalla1.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:rodriguez/pantalla1.dart';
 import 'package:rodriguez/pantalla2.dart';
 
-void main() => runApp(RutasPaginas());
-
-class RutasPaginas extends StatelessWidget {
-  const RutasPaginas({Key? key}) : super(key: key);
+class Pantalla1 extends StatelessWidget {
+  const Pantalla1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      debugShowCheckedModeBanner: false,
-      title: "App entre pagina",
-      initialRoute: "/",
-      routes: {
-        "/": (context) => Pantalla1(),
-        "/pantalla2": (context) => Pantalla2(),
-      }, //navegacion entre paginas
-    ); //fin materal
-  } //clase ruta pagina
-} //fin RutasPaginas
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pantalla1 Valeria Rodriguez"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Align(
+            alignment: Alignment.center,
+          ),
+          Text("Inicio de la pagina", style: TextStyle(fontSize: 20)),
+          Icon(
+            Icons.filter_drama,
+            size: 70,
+            color: Colors.orange[300],
+          ),
+          ElevatedButton(
+            child: const Text("Vamos a la pantalla2"),
+            onPressed: () {
+              Navigator.pushNamed(context, "/pantalla2",
+                  arguments: "Mensaje desde la pantalla1");
+            },
+          ),
+          Card(
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Tarjeta Rodriguez",
+                  style: TextStyle(fontSize: 30, color: Color(0xfff9f9f9)),
+                ),
+              ),
+              color: Color(0xff5e1918))
+        ],
+      ),
+    );
+  } //widget
+} //Fiin clase pantalla 1
